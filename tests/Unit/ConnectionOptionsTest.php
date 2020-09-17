8158b6797336132830cf3e2c7bf8cabbb13eea43
Harm van Tilborg <harm.van.tilborg@on2it.net>
2020-09-17 12:27:51 +0200

    Added "explicitClose" option to avoid closing connection implicitly in `Connection`'s destructor

--- tests/Unit/ConnectionOptionsTest.php
+++ tests/Unit/ConnectionOptionsTest.php
@@ -3,20 +3,33 @@
 class ConnectionOptionsTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testConstructorDefaults()
+    {
+        $opts = new \NatsStreaming\ConnectionOptions();
+
+        $this->assertEquals('', $opts->getDiscoverPrefix());
+        $this->assertEquals('', $opts->getClientID());
+        $this->assertEquals('test-cluster', $opts->getClusterID());
+        $this->assertFalse($opts->getExplicitClose());
+
+        $this->assertNull($opts->getNatsOptions());
+    }
+
     public function testConstructor()
     {
-
         $opts = new \NatsStreaming\ConnectionOptions([
             'discoverPrefix' => 'foo',
             'clientID' => 'bar',
             'clusterID' =>'baz',
+            'explicitClose' => true,
             'foobar' => 'baz',
-            'natsOptions' => new \Nats\ConnectionOptions(['host' => 'hosty'])
+            'natsOptions' => new \Nats\ConnectionOptions(['host' => 'hosty']),
         ]);
 
         $this->assertEquals('foo', $opts->getDiscoverPrefix());
         $this->assertEquals('bar', $opts->getClientID());
         $this->assertEquals('baz', $opts->getClusterID());
+        $this->assertTrue($opts->getExplicitClose());
 
         $this->assertEquals('hosty', $opts->getNatsOptions()->getHost());
     }
